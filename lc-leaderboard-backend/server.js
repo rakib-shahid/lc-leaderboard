d6e2156c47db275392d8f6279454aac639bb79af
rakib <rakibshahid1298@gmail.com>
2024-10-01 01:10:33 +0000

    latest backend server

--- lc-leaderboard-backend/server.js
+++ lc-leaderboard-backend/server.js
@@ -5,6 +5,7 @@ const { Pool } = require("pg");
 const querystring = require("querystring");
 const fs = require("fs");
 const https = require("https");
+const NodeCache = require("node-cache"); // Import node-cache
 require("dotenv").config();
 
 const app = express();
@@ -18,6 +19,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
+
+// Initialize cache
+const cache = new NodeCache({ stdTTL: 1800 }); // Cache TTL of 1 hour
 
 app.use(
   session({
@@ -175,12 +179,13 @@ app.post("/register", async (req, res) => {
 app.get("/leaderboard", async (req, res) => {
   try {
     const result = await pool.query(`
-        SELECT u.username, p.points, ao.discord_username
+        SELECT ao.leetcode_username username, p.points, ao.discord_username
         FROM points p
         JOIN users u ON p.user_id = u.id
-        JOIN account_owner ao ON ao.leetcode_username = u.username
+        JOIN account_owner ao ON LOWER(ao.leetcode_username) = LOWER(u.username)
         ORDER BY p.points DESC
-        LIMIT 50;
+        LIMIT 15;
+
       `);
     res.json(result.rows);
   } catch (error) {
@@ -209,11 +214,20 @@ app.get("/api/leetcode-stats", async (req, res) => {
 
     const leetcodeUsername = result.rows[0].leetcode_username;
 
+    // Check cache for data
+    const cachedData = cache.get(leetcodeUsername);
+    if (cachedData) {
+      return res.json(cachedData); // Return cached data
+    }
+
     // Fetch data from external LeetCode API
     const response = await fetch(
       `https://alfa-leetcode-api.onrender.com/${leetcodeUsername}`
     );
     const data = await response.json();
+
+    // Cache the response
+    cache.set(leetcodeUsername, data);
 
     res.json(data);
   } catch (error) {
@@ -232,9 +246,9 @@ app.get("/api/discord_lookup", async (req, res) => {
   }
 
   try {
-    // Query the database to find the LeetCode username for the given Discord username
+    // Query the database to find the LeetCode username for the given Discord username (case-insensitive)
     const result = await pool.query(
-      "SELECT leetcode_username FROM account_owner WHERE discord_username = $1",
+      "SELECT leetcode_username FROM account_owner WHERE LOWER(discord_username) = LOWER($1)",
       [discordUsername]
     );
 
@@ -247,6 +261,15 @@ app.get("/api/discord_lookup", async (req, res) => {
 
     const leetcodeUsername = result.rows[0].leetcode_username;
 
+    // Check cache for data
+    const cachedData = cache.get(leetcodeUsername);
+    if (cachedData) {
+        if (!cachedData["message"]){
+            cachedData.cache = "true";
+            return res.json(cachedData); // Return cached data
+        }
+    }
+
     // Fetch LeetCode stats from external API
     const response = await fetch(
       `https://alfa-leetcode-api.onrender.com/${leetcodeUsername}`
@@ -255,24 +278,42 @@ app.get("/api/discord_lookup", async (req, res) => {
     let localrank = -1;
     // Calculate the local ranking of the user in the points table
     const ranking_result = await pool.query(
-      `SELECT ranking FROM (SELECT user_id, points, RANK() OVER (ORDER BY points DESC) AS ranking FROM points) ranked_points JOIN users ON ranked_points.user_id = users.id WHERE users.username = '${leetcodeUsername}';`
+      `SELECT ranking FROM (SELECT user_id, points, RANK() OVER (ORDER BY points DESC) AS ranking FROM points) ranked_points JOIN users ON ranked_points.user_id = users.id WHERE LOWER(users.username) = LOWER($1);`,
+      [leetcodeUsername]
     );
     if (ranking_result.rows.length != 0) {
       localrank = ranking_result.rows[0].ranking;
     }
 
-    // Send the LeetCode stats
-    res.json({
-      leetcode_username: data.username,
-      ranking: data.ranking,
-      local_ranking: localrank,
-      avatar: data.avatar,
-    });
+    const wins = await pool.query(
+        `SELECT wins FROM points WHERE user_id = (SELECT id FROM users WHERE LOWER(username) = LOWER($1));`,
+          [leetcodeUsername]
+      );
+      let winCount = 0;
+      if (wins.rows.length != 0) {
+        if (wins.rows[0].wins != null){
+        winCount = wins.rows[0].wins;
+
+        }
+      }
+  
+      // Send the LeetCode stats
+      const result_json = {
+        discord_username: discordUsername,
+        leetcode_username: data.username,
+        ranking: data.ranking,
+        local_ranking: localrank,
+          wins: winCount,
+        avatar: data.avatar,
+      };
+    cache.set(discordUsername, result_json);
+    res.json(result_json);
   } catch (error) {
-    console.error("Error fetching LeetCode stats:", error);
+    console.log("Error fetching LeetCode stats:", error);
     res.status(500).json({ message: "Error fetching LeetCode stats" });
   }
 });
+
 
 app.get("/api/leetcode_lookup", async (req, res) => {
   const leetcodeUsername = req.headers["leetcode-username"];
@@ -284,19 +325,75 @@ app.get("/api/leetcode_lookup", async (req, res) => {
   }
 
   try {
+    // Check cache for data
+    const cachedData = cache.get(leetcodeUsername);
+    if (cachedData) {
+        if (!cachedData["message"]){
+            cachedData.cache = "true";
+            return res.json(cachedData); // Return cached data
+        }
+    }
+
+    // Query to find Discord username (case-insensitive)
+    const result = await pool.query(
+      "SELECT discord_username FROM account_owner WHERE LOWER(leetcode_username) = LOWER($1)",
+      [leetcodeUsername]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message:
+          "LeetCode username not found for the provided Discord username",
+      });
+    }
+
+    const discordUsername = result.rows[0].discord_username;
+
     // Fetch LeetCode stats from external API
     const response = await fetch(
       `https://alfa-leetcode-api.onrender.com/${leetcodeUsername}`
     );
     const data = await response.json();
 
+    let localrank = -1;
+    // Calculate the local ranking of the user in the points table (case-insensitive)
+    const ranking_result = await pool.query(
+      `SELECT ranking FROM (SELECT user_id, points, RANK() OVER (ORDER BY points DESC) AS ranking FROM points) ranked_points JOIN users ON ranked_points.user_id = users.id WHERE LOWER(users.username) = LOWER($1);`,
+      [leetcodeUsername]
+    );
+    if (ranking_result.rows.length != 0) {
+      localrank = ranking_result.rows[0].ranking;
+    }
+
+    const wins = await pool.query(
+      `SELECT wins FROM points WHERE user_id = (SELECT id FROM users WHERE LOWER(username) = LOWER($1));`,
+        [leetcodeUsername]
+    );
+    let winCount = 0;
+    if (wins.rows.length != 0) {
+        if (wins.rows[0].wins != null){
+        winCount = wins.rows[0].wins;
+
+        }
+      }
+
     // Send the LeetCode stats
-    res.json(data);
+    const result_json = {
+      discord_username: discordUsername,
+      leetcode_username: data.username,
+      ranking: data.ranking,
+      local_ranking: localrank,
+        wins: winCount,
+      avatar: data.avatar,
+    };
+    cache.set(leetcodeUsername, result_json);
+    res.json(result_json);
   } catch (error) {
-    console.error("Error fetching LeetCode stats:", error);
+    console.log("Error fetching LeetCode stats:", error);
     res.status(500).json({ message: "Error fetching LeetCode stats" });
   }
 });
+
 
 app.get("/api/leetcode_ac", async (req, res) => {
   const leetcodeUsername = req.headers["leetcode-username"];
@@ -308,13 +405,22 @@ app.get("/api/leetcode_ac", async (req, res) => {
   }
 
   try {
+    // Check cache for data
+    const cachedData = cache.get(`${leetcodeUsername}_ac`);
+    if (cachedData) {
+      return res.json(cachedData); // Return cached data
+    }
+
     // Fetch LeetCode stats from external API
     const response = await fetch(
       `https://alfa-leetcode-api.onrender.com/${leetcodeUsername}/acSubmission`
     );
     const data = await response.json();
 
-    //   send the data response
+    // Cache the response
+    cache.set(`${leetcodeUsername}_ac`, data);
+
+    // Send the data response
     res.json(data);
   } catch (error) {
     console.error("Error fetching LeetCode stats:", error);
@@ -322,6 +428,7 @@ app.get("/api/leetcode_ac", async (req, res) => {
   }
 });
 
+// Other endpoints...
 app.get("/polls", async (req, res) => {
   try {
     const data = fs.readFileSync("data.json", "utf8");
@@ -329,6 +436,16 @@ app.get("/polls", async (req, res) => {
   } catch (error) {
     console.error("Error reading data.json:", error);
     res.status(500).json({ message: "Error reading data.json" });
+  }
+});
+
+app.get("/news", async (req, res) => {
+  try {
+    const data = fs.readFileSync("news.json", "utf8");
+    res.json(JSON.parse(data));
+  } catch (error) {
+    console.error("Error reading news.json:", error);
+    res.status(500).json({ message: "Error reading news.json" });
   }
 });
 
