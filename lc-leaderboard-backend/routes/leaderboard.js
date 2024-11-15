7109992d472363ca6b75f2ad6b220d33d6d413b0
rakib <rakibshahid1298@gmail.com>
2024-11-15 21:04:32 +0000

    fixed history endpoint

--- lc-leaderboard-backend/routes/leaderboard.js
+++ lc-leaderboard-backend/routes/leaderboard.js
@@ -18,5 +18,24 @@ module.exports = (pool) => {
       res.status(500).json({ message: "Error fetching leaderboard" });
     }
   });
+  router.get('/leaderboard_history', async (req, res) => {
+    try {
+      const result = await pool.query(`
+          SELECT ao.discord_username,
+                 ao.leetcode_username,
+                 p.total_points,
+                 p.wins AS total_wins
+          FROM points p
+          JOIN users u ON p.user_id = u.id
+          JOIN account_owner ao ON LOWER(ao.leetcode_username) = LOWER(u.username)
+          ORDER BY p.wins DESC, p.total_points DESC;
+      `);
+  
+      res.json(result.rows);
+    } catch (error) {
+      console.error("Error fetching historical leaderboard:", error);
+      res.status(500).json({ message: "Error fetching historical leaderboard" });
+    }
+  });
   return router;
 };
